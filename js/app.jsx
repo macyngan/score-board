cef4537d7bc7d0afa64c4dd9cdbfe4873ed9d107
Macy Ngan <macy@Macys-MacBook-Air.local>
2017-11-03 00:36:44 -0700

    score board ready

---
+++ js/app.jsx
@@ -0,0 +1,17 @@
+var PLAYERS = [
+  {
+    name: "Jim Hoskins",
+    score: 31,
+    id: 1,
+  },
+  {
+    name: "Andrew Chalkley",
+    score: 35,
+    id: 2,
+  },
+  {
+    name: "Alena Holligan",
+    score: 42,
+    id: 3,
+  },
+];
